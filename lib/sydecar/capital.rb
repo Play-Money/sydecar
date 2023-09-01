55f21002f12fb0d8941713de3670aeb344f7f4fc
VitaliiShevchenko <iksel@ukr.net>
2023-09-01 23:47:53 +0300

    PLAY-162: Added methods create_SPV_capital_call and fetch_SPV_capital_call

--- lib/sydecar/capital.rb
+++ lib/sydecar/capital.rb
@@ -49,11 +49,119 @@ module Sydecar
   	  #   }
   	  # }
   	  # @return See example RESPONSES: > '200'
+		  #
+		  # See on the https://api-docs.sydecar.io/api/#tag/Capital-Call-Management/operation/getAllCapitalCallEvents
   	  def fetch_capital_calls_for_SPV(params: {}, body: {})
   		  query = '?'
   		  query += URI.encode_www_form(params)
   		  Connection.instance.post("#{CAPITAL_CALL_EVENTS_URL}#{query}", body)
-  	  end
+	    end
+
+		  # This method create an SPV capital call event
+		  # @param
+		  #   HEADER PARAMETERS:
+		  #   - idempotency-key [String]
+		  #   REQUEST BODY SCHEMA: application/json
+		  #   - {
+		  #   "capital_call_events": [ (required)
+		  #     {
+		  #       "percentage": 100,   (required 0..100)
+		  #       "memo": "string"
+		  #     }
+		  #   ],
+		  #   "spv_id": "string"       (required)
+		  # }
+		  #     - Create one or more capital call events. A valid SPV id is needed.
+		  # @example
+		  #   RESPONSE SAMPLE
+		  #   {
+		  #   "data": [
+		  #     {
+		  #       "id": "string",
+		  #       "created_at": "2019-08-24T14:15:22Z",
+		  #       "updated_at": "2019-08-24T14:15:22Z",
+		  #       "spv_id": "string",
+		  #       "memo": "string",
+		  #       "percentage": 0,
+		  #       "capital_calls": [
+		  #         {
+		  #           "id": "string",
+		  #           "created_at": "2019-08-24T14:15:22Z",
+		  #           "updated_at": "2019-08-24T14:15:22Z",
+		  #           "amount": 0,
+		  #           "amount_funded": 0,
+		  #           "due_date": "2019-08-24T14:15:22Z",
+		  #           "stage": "CREATED",
+		  #           "subscription_id": "string",
+		  #           "capital_call_event_id": "string"
+		  #         }
+		  #       ]
+		  #     }
+		  #   ]
+		  # }
+		  # More detail see on the
+		  # https://api-docs.sydecar.io/api/#tag/Capital-Call-Management/operation/createOneOrMoreCapitalCallEvents
+		  # @return [Hash] Responses: > '201'
+		  def create_SPV_capital_call(body:, idempotency_key:)
+			  Connection.instance.post("#{CAPITAL_CALL_EVENTS_URL}/create", body, { 'idempotency-key': idempotency_key })
+		  end
+
+		  # Fetch an SPV capital call event by id
+		  # @param
+		  #   PATH PARAMETERS
+		  #   - 'capital_call_event_id' [String] (required)
+		  #   QUERY PARAMETERS
+		  #   - 'include' [String] (Value: "capital_calls")
+		  # @EXAMPLE
+		  #   Response samples:
+		  #   {
+		  #   "id": "string",
+		  #   "created_at": "2019-08-24T14:15:22Z",
+		  #   "updated_at": "2019-08-24T14:15:22Z",
+		  #   "spv_id": "string",
+		  #   "memo": "string",
+		  #   "percentage": 0,
+		  #   "capital_calls": [
+		  #     {
+		  #       "id": "string",
+		  #       "created_at": "2019-08-24T14:15:22Z",
+		  #       "updated_at": "2019-08-24T14:15:22Z",
+		  #       "amount": 0,
+		  #       "amount_funded": 0,
+		  #       "due_date": "2019-08-24T14:15:22Z",
+		  #       "stage": "CREATED",
+		  #       "subscription_id": "string",
+		  #       "capital_call_event_id": "string"
+		  #     }
+		  #   ]
+		  # }
+		  # @return [Hash] see example
+		  #   Response: > '200'
+		  def fetch_SPV_capital_call(capital_call_event_id:, query: {})
+			  query = "?#{URI.encode_www_form(query)}"
+			  Connection.instance.get("#{CAPITAL_CALL_EVENTS_URL}/#{capital_call_event_id}#{query}")
+		  end
+
+		  # Fetch an SPV capital call event by id
+		  # @param
+		  #   PATH PARAMETERS
+		  #   - 'capital_call_event_id' [String] (required)
+		  #   REQUEST BODY SCHEMA: application/json
+		  #   - 'percentage' [Number] [ 0 .. 100 ]
+		  #   - 'memo'       [String]
+		  # @EXAMPLE
+		  #   Response samples:
+		  #   {
+		  #   "id": "string",
+		  #   "created_at": "2019-08-24T14:15:22Z",
+		  #   "updated_at": "2019-08-24T14:15:22Z",
+		  #   "spv_id": "string",
+		  #   "memo": "string",
+		  #   "percentage": 0
+		  #   }
+		  # @return [Hash] see upper
+		  #   Response: > '200'
+
 	  end
   end
 end
--- spec/lib/sydecar/capital_spec.rb
+++ spec/lib/sydecar/capital_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 RSpec.describe Sydecar::Capital do
 	let!(:headers) { Sydecar::Connection.headers }
 	let!(:body) { {}.to_json }
+	let!(:capital_call_event_id) { '1' }
 
 	it 'calls method fetch_capital_calls_for_SPV' do
 		stub_request(:post, "#{Sydecar::Connection.base_url}/#{Sydecar::Capital::CAPITAL_CALL_EVENTS_URL}")
@@ -11,5 +12,21 @@ RSpec.describe Sydecar::Capital do
 			.to_return(body: body, status: 200)
 
 		subject.class.fetch_capital_calls_for_SPV(params: {}, body: {})
+	end
+
+	it 'calls method create_SPV_capital_call' do
+		stub_request(:post, "#{Sydecar::Connection.base_url}/#{Sydecar::Capital::CAPITAL_CALL_EVENTS_URL}/create")
+			.with( body: body, headers: headers)
+			.to_return(body: body, status: 201)
+
+		subject.class.create_SPV_capital_call(body: body, idempotency_key: 'unique')
+	end
+
+	it 'calls method fetch_SPV_capital_call' do
+		stub_request(:get, "#{Sydecar::Connection.base_url}/#{Sydecar::Capital::CAPITAL_CALL_EVENTS_URL}/#{capital_call_event_id}?include=capital_calls")
+			.with( headers: headers)
+			.to_return(body: body, status: 200)
+
+		subject.class.fetch_SPV_capital_call(capital_call_event_id: capital_call_event_id, query: { include: 'capital_calls' })
 	end
 end
