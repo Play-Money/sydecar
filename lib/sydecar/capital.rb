c95558cb11d993e7553e3f265a0fb92393956a34
VitaliiShevchenko <iksel@ukr.net>
2023-09-04 19:31:54 +0300

    PLAY-162: Little fixed capital.rb (a try number 4)

--- lib/sydecar/capital.rb
+++ lib/sydecar/capital.rb
@@ -6,49 +6,49 @@ module Sydecar
     CAPITAL_CALL_EVENTS_URL = 'v1/capital_call_events'
     class << self
       # Fetch all capital call events for an SPV
-       # @param
-       #   Query parameters [Hash]
-       #   - 'sort' [String] (Enum: 'asc', 'desc')
-       #   - 'limit' [Number] (<=100)
-       #   - 'offset' [Number]
-       #   - 'start_date' [String]
-       #   - 'end_date' [String]
-       #   - 'include' [String] (Value: 'capital_calls')
-       #   REQUEST BODY SCHEMA: application/json
-       #   - 'ids' [Array of string]
-       # @example
-       # RESPONSE DATA (Sample):
-       #   {
-       #   "data": [
-       #     {
-       #       "id": "string",
-       #       "created_at": "2019-08-24T14:15:22Z",
-       #       "updated_at": "2019-08-24T14:15:22Z",
-       #       "spv_id": "string",
-       #       "memo": "string",
-       #       "percentage": 0,
-       #       "capital_calls": [
-       #         {
-       #           "id": "string",
-       #           "created_at": "2019-08-24T14:15:22Z",
-       #           "updated_at": "2019-08-24T14:15:22Z",
-       #           "amount": 0,
-       #           "amount_funded": 0,
-       #           "due_date": "2019-08-24T14:15:22Z",
-       #           "stage": "CREATED",
-       #           "subscription_id": "string",
-       #           "capital_call_event_id": "string"
-       #         }
-       #       ]
-       #     }
-       #   ],
-       #   "pagination": {
-       #     "limit": 0,
-       #     "offset": 0,
-       #     "total": 0
-       #   }
-       # }
-       # @return See example RESPONSES: > '200'
+      # @param
+      #   Query parameters [Hash]
+      #   - 'sort' [String] (Enum: 'asc', 'desc')
+      #   - 'limit' [Number] (<=100)
+      #   - 'offset' [Number]
+      #   - 'start_date' [String]
+      #   - 'end_date' [String]
+      #   - 'include' [String] (Value: 'capital_calls')
+      #   REQUEST BODY SCHEMA: application/json
+      #   - 'ids' [Array of string]
+      # @example
+      # RESPONSE DATA (Sample):
+      #   {
+      #   "data": [
+      #     {
+      #       "id": "string",
+      #       "created_at": "2019-08-24T14:15:22Z",
+      #       "updated_at": "2019-08-24T14:15:22Z",
+      #       "spv_id": "string",
+      #       "memo": "string",
+      #       "percentage": 0,
+      #       "capital_calls": [
+      #         {
+      #           "id": "string",
+      #           "created_at": "2019-08-24T14:15:22Z",
+      #           "updated_at": "2019-08-24T14:15:22Z",
+      #           "amount": 0,
+      #           "amount_funded": 0,
+      #           "due_date": "2019-08-24T14:15:22Z",
+      #           "stage": "CREATED",
+      #           "subscription_id": "string",
+      #           "capital_call_event_id": "string"
+      #         }
+      #       ]
+      #     }
+      #   ],
+      #   "pagination": {
+      #     "limit": 0,
+      #     "offset": 0,
+      #     "total": 0
+      #   }
+      # }
+      # @return See example RESPONSES: > '200'
       #
       # See on the https://api-docs.sydecar.io/api/#tag/Capital-Call-Management/operation/getAllCapitalCallEvents
        def fetch_capital_calls_for_SPV(params: {}, body: {})
