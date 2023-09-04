daa22cdef4dcd759f0f5850599a8613716618827
VitaliiShevchenko <iksel@ukr.net>
2023-09-04 20:43:03 +0300

    PLAY-163: Added fetch all webhook

--- lib/sydecar/webhook_calls.rb
+++ lib/sydecar/webhook_calls.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Sydecar
-	CREATE_URL = 'v1/configure/webhooks/create'
+	URL = 'v1/configure/webhooks/'
 	class WebhookCalls
 		class << self
 			# The method create a webhook callback URL
@@ -14,6 +14,12 @@ module Sydecar
 			#     - token [String]     - For security reasons every webhook is associated with a token.  (required)
 			#                             This token will be used to sign the incoming payloads.
 			#     - disabled [boolean] - Enable / disable your web hook.                             (required)
+			#
+			# @note
+			#   Set a webhook callback URL. You can provide any URL for Sydecar webhook callbacks to land.
+			#   All webhook callbacks for the specified environment will be directed to the URL you provide.
+			#   For maximum security, make sure to use https endpoints. In addition, webhook calls will be signed with a token.
+			#
 			# @example
 			#   Response samples:
 			#   {
@@ -27,8 +33,12 @@ module Sydecar
 			# }
 			# @return [Hash] - Responses: > '201'
 			def create(body:, idempotency_key:)
-				Connection.instance.post(CREATE_URL, body, { 'idempotency-key': idempotency_key })
+				Connection.instance.post("#{URL}create", body, { 'idempotency-key': idempotency_key })
 			end
+
+			# This method fetch all webhooks
+			# @param
+			#   QUERY PARAMETERS
 		end
 	end
 end
