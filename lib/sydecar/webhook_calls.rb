80e3b9e460a0869feb7891fdcb1d2b9778814012
VitaliiShevchenko <iksel@ukr.net>
2023-09-05 10:00:21 +0300

    PLAY-163: Added remaining methods

--- lib/sydecar/webhook_calls.rb
+++ lib/sydecar/webhook_calls.rb
@@ -69,11 +69,125 @@ module Sydecar
       # }
       # @return See example RESPONSES: > '200'
       #
-      # See on the hhttps://api-docs.sydecar.io/api/#tag/Configuration/operation/getAllWebhooks
-      def fetch_all_webhooks(params:)
-      	query = '?'
-      	query += URI.encode_www_form(params)
-      	Connection.instance.post("#{URL}#{query}")
+      # See on the https://api-docs.sydecar.io/api/#tag/Configuration/operation/getAllWebhooks
+      def fetch_all_webhooks(query:)
+      	query = URI.encode_www_form(query)
+      	Connection.instance.post("#{URL}?#{query}")
+      end
+
+      # This method Fetch webhook information by id
+      # Fetch webhook information by id. This does not include the associate webhook events.
+      # @param
+      #   PATH PARAMETERS
+      #   - webhook_id [String] (required)
+      # Response samples
+      #   {
+      #   "id": "string",
+      #   "created_at": "2019-08-24T14:15:22Z",
+      #   "updated_at": "2019-08-24T14:15:22Z",
+      #   "url": "string",
+      #   "label": "string",
+      #   "token": "string",
+      #   "disabled": true
+      # }
+      # #return [Hash] Responses: > '200'
+      #
+      # See on the https://api-docs.sydecar.io/api/#tag/Configuration/operation/getWebhook
+      def fetch_webhook(webhook_id:)
+	      Connection.instance.get("#{URL}#{webhook_id}")
+      end
+
+      # This method Update a webhook
+      # Update a webhook. You can update the label, url and/or token.
+      # @param
+      #   PATH PARAMETERS
+      #   - webhook_id [String] (required)
+      #   REQUEST BODY SCHEMA: application/json
+      #     - url [String]       - The callback url associated the the webhook                     (required)
+      #     - label [String]     - A label to easily identify your web hook                        (required)
+      #     - token [String]     - For security reasons every webhook is associated with a token.  (required)
+      #                             This token will be used to sign the incoming payloads.
+      #     - disabled [boolean] - Enable / disable your web hook.                             (required)
+      # @example
+      #   Response samples:
+      #   {
+      #   "id": "string",
+      #   "created_at": "2019-08-24T14:15:22Z",
+      #   "updated_at": "2019-08-24T14:15:22Z",
+      #   "url": "string",
+      #   "label": "string",
+      #   "token": "string",
+      #   "disabled": true
+      # }
+      # @return [Hash] - Responses: > '200'
+      #
+      # See on the https://api-docs.sydecar.io/api/#tag/Configuration/operation/updateWebhook
+      def update_webhook(webhook_id:, body:)
+	      Connection.instance.patch("#{URL}#{webhook_id}", body)
+      end
+
+      # This method Fetch latest webhook events
+      # Fetch the latest events for a webhook.
+      # @param
+      #   PATH PARAMETERS
+      #   - webhook_id [String] (required)
+      #   Query parameters [Hash]
+      #   - 'sort' [String] (Enum: 'asc', 'desc')
+      #   - 'limit' [Number] (<=100)
+      #   - 'offset' [Number]
+      #   - 'start_date' [String]
+      #   - 'end_date' [String]
+      # @example
+      # RESPONSE DATA (Sample):
+      # {
+      #   "data": [
+      #     {
+      #       "id": "string",
+      #       "created_at": "2019-08-24T14:15:22Z",
+      #       "updated_at": "2019-08-24T14:15:22Z",
+      #       "type": "plaid.account.item.expiring",
+      #       "details": {
+      #         "property1": "string",
+      #         "property2": "string"
+      #       }
+      #     }
+      #   ],
+      #   "pagination": {
+      #     "limit": 0,
+      #     "offset": 0,
+      #     "total": 0
+      #   }
+      # }
+      # @return See example RESPONSES: > '200'
+      #
+      # See on the https://api-docs.sydecar.io/api/#tag/Configuration/operation/getAllWebhookEvents
+      def fetch_latest_webhook_events(webhook_id:, query: {})
+	      query = URI.encode_www_form(query)
+	      Connection.instance.post("#{URL}?#{query}/events")
+      end
+
+      # This method Resend webhook event
+      # Resend a webhook event which was previously published.
+      # @param
+      #   PATH PARAMETERS
+      #   - event_id [String] (required)
+      # @example
+      #   Response samples
+      #   {
+      #   "id": "string",
+      #   "created_at": "2019-08-24T14:15:22Z",
+      #   "updated_at": "2019-08-24T14:15:22Z",
+      #   "type": "plaid.account.item.expiring",
+      #   "details": {
+      #     "property1": "string",
+      #     "property2": "string"
+      #   }
+      # }
+      # @return [Hash] Responses > '200'
+      #
+      # See on the https://api-docs.sydecar.io/api/#tag/Configuration/operation/replayWebhookEvent
+      def resend_webhook_event(event_id:)
+	      Connection.instance.post("#{URL}/event/#{event_id}")
       end
 		end
 	end
